3c9b40e183798990dc0a41d5c25a8df244679e1d
DaniyarAA <gitmailhub@gmail.com>
2024-05-20 12:54:00 +0600

    Реализовал шаблон проектирования Состояние

--- ToDoApp/src/enums/Status.java
+++ ToDoApp/src/enums/Status.java
@@ -1,58 +1,71 @@
 package enums;
 
+import model.Task;
+
 public enum Status {
-    NEW("Новый"){
+    NEW("НОВЫЙ") {
         @Override
-        public void changeStatus() {
-
+        public void changeStatus(Task task) {
+            if (task.getStatus() == NEW) {
+                System.out.println("Статус задачи изменен на - В ПРОЦЕССЕ");
+                task.setStatus(IN_PROGRESS);
+            } else {
+                throw new IllegalStateException("Статус задачи можно изменить на 'В ПРОЦЕССЕ' только из статуса 'НОВЫЙ'");
+            }
         }
 
         @Override
-        public void changeDescription() {
-
+        public void changeDescription(Task task, String description) {
+            task.setDescription(description);
         }
 
         @Override
-        public void deleteTask() {
-
-        }
-    },
-    IN_PROGRESS("В процессе"){
-
-        @Override
-        public void changeStatus() {
-
-        }
-
-        @Override
-        public void changeDescription() {
-
-        }
-
-        @Override
-        public void deleteTask() {
-
+        public void deleteTask(Task task) {
+            System.out.println("Задача удалена");
+//            taskManager.deleteTask(task);
         }
     },
-    DONE("Сделано"){
+    IN_PROGRESS("В ПРОЦЕССЕ") {
         @Override
-        public void changeStatus() {
-
+        public void changeStatus(Task task) {
+            if (task.getStatus() == IN_PROGRESS) {
+                System.out.println("Статус задачи изменен на - СДЕЛАНО");
+                task.setStatus(DONE);
+            } else {
+                throw new IllegalStateException("Статус задачи можно изменить на 'СДЕЛАНО' только из статуса 'В ПРОЦЕССЕ'");
+            }
         }
 
         @Override
-        public void changeDescription() {
-
+        public void changeDescription(Task task, String description) {
+            throw new IllegalStateException("Нельзя изменить описание задачи со статусом 'В ПРОЦЕССЕ'");
         }
 
         @Override
-        public void deleteTask() {
+        public void deleteTask(Task task) {
+            throw new IllegalStateException("Нельзя удалить задачу со статусом 'В ПРОЦЕССЕ'");
+        }
+    },
+    DONE("СДЕЛАНО") {
+        @Override
+        public void changeStatus(Task task) {
+            throw new IllegalStateException("Нельзя изменить статус со статуса 'СДЕЛАНО'");
+        }
 
+        @Override
+        public void changeDescription(Task task, String description) {
+            throw new IllegalStateException("Нельзя изменить описание задачи со статусом 'СДЕЛАНО'");
+        }
+
+        @Override
+        public void deleteTask(Task task) {
+            throw new IllegalStateException("Нельзя удалить задачу со статусом 'СДЕЛАНО'");
         }
     };
+
     private final String description;
 
-    Status(String description){
+    Status(String description) {
         this.description = description;
     }
 
@@ -60,7 +73,7 @@ public enum Status {
         return description;
     }
 
-    public abstract void changeStatus();
-    public abstract void changeDescription();
-    public abstract void deleteTask();
+    public abstract void changeStatus(Task task);
+    public abstract void changeDescription(Task task, String description);
+    public abstract void deleteTask(Task task);
 }
